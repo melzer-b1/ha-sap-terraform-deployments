b433fe66fad1706a93616a7470ddd4af93d329a7
Jonas Melzer <melzer@b1-systems.de>
2022-02-15 14:42:38 +0100

    libvirt: upgrade to terraform 1.1.X and dmacvicar/libvirt 0.6.14

--- libvirt/modules/hana_node/main.tf
+++ libvirt/modules/hana_node/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.1.0"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.6.3"
+      version = "0.6.14"
     }
   }
 }
@@ -99,7 +99,7 @@ resource "libvirt_domain" "hana_domain" {
     autoport    = true
   }
 
-  cpu = {
+  cpu {
     mode = "host-passthrough"
   }
 }
